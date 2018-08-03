9e4bd548334c5cd70fe295e07ccab5508cf5cd92
Crystal Agerton <crystal.agerton2015@gmail.com>
2018-08-03 15:20:42 -0700

    Added New Powers
    Powers will be playtested to determine which is the best/most intuitive

--- Assets/Scripts/PlayerPowers.cs
+++ Assets/Scripts/PlayerPowers.cs
@@ -12,7 +12,15 @@ public class PlayerPowers : MonoBehaviour {
     public GameObject glow;
     public GameObject partner;
 
-    private int range = 5;
+    public int limit = 5;
+    public powerType power;
+
+
+    public enum powerType
+    {
+        BigWhenApartLimited, BigWhenApartUnlimited,
+        BigWhenTogether, BigWhenBalanced
+    }
 	
 	void Update () {
         handleGlowGrowth();
@@ -20,33 +28,84 @@ public class PlayerPowers : MonoBehaviour {
 
     void handleGlowGrowth()
     {
-        #region Distance Code - Limited Ver
-        /*
-         * Concerning the below code:
-         * Places a limit to how big the powers get
-         * Otherwise the powers will grow based on the distance between them
-         */
-        if (!partnerInRange(range) && glow.transform.localScale.x < range)
-            glow.transform.localScale += new Vector3(1, 1, 0);
-        else if (partnerInRange(range))
+        switch (power)
+        {
+            case powerType.BigWhenApartLimited:
+                apartGrowthLimited();
+                break;
+            case powerType.BigWhenApartUnlimited:
+                apartGrowthUnlimited();
+                break;
+            case powerType.BigWhenTogether:
+                togetherGrowth();
+                break;
+            case powerType.BigWhenBalanced:
+                balancedGrowth();
+                break;
+            default:
+                break;
+        }
+    }
+
+    #region Growth Functions
+    void apartGrowthLimited()
+    {
+        /// Places a limit to how big the powers get
+        /// Otherwise the powers will grow based on the distance between them
+
+        if (partnerInRange(limit))
         {
             float distance = getDistanceFromPartner();
             glow.transform.localScale = new Vector3(distance, distance, 0);
         }
-        #endregion
-
-        #region Distance Code - Unlimited Ver
-        /*
-         * Concerning the below code:
-         * Does not place alimit to how big the powers can get
-         * Results in powers that always cover the distance between players
-         */
-        //float distance = getDistanceFromPartner();
-        //glow.transform.localScale = new Vector3(distance, distance, 0);
-        #endregion
+        else if (glow.transform.localScale.x < limit)
+        {
+            glow.transform.localScale += new Vector3(1, 1, 0);
+        }
     }
 
+    void apartGrowthUnlimited()
+    {
+        /// Does not place a limit to how big the powers can get
+        /// Results in powers that always cover the distance between players
 
+        float distance = getDistanceFromPartner();
+        glow.transform.localScale = new Vector3(distance, distance, 0);
+    }
+
+    void togetherGrowth()
+    {
+        /// Requires players to be near each other to use their powers
+        /// The closer they are, the more effective their powers are
+
+        float distance = getDistanceFromPartner();
+        if (1 / distance * limit >= 1)
+            glow.transform.localScale = new Vector3(1 / distance * limit, 1 / distance * limit, 0);
+    }
+
+    void balancedGrowth()
+    {
+        /// Requires players to maintain a balanced distance to use their powers
+        /// If they move too far, they won't be able to use their powers
+        /// If they are too close, they loose some power
+
+        float distance = getDistanceFromPartner();
+
+        if (partnerInRange(limit))
+        {
+            glow.transform.localScale = new Vector3(distance, distance, 0);
+        }
+        else
+        {
+            float diff = 0;
+            if (distance - limit <= limit - diff)
+                diff = limit - (distance - limit);
+            glow.transform.localScale = new Vector3(diff, diff, 0);
+        }
+    }
+    #endregion
+
+    #region Helper Functions
     bool partnerInRange(int targetRange)
     {
         float distance = getDistanceFromPartner();
@@ -67,4 +126,5 @@ public class PlayerPowers : MonoBehaviour {
         Vector3 difference = position1 - position2;
         return difference.magnitude;
     }
+    #endregion
 }
