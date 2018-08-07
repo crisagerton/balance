bb003f1968800a32354706592133fceb2981fb43
Crystal Agerton <crystal.agerton2015@gmail.com>
2018-08-07 15:32:09 -0700

    Added Player Specific Doors

---
+++ Assets/Scripts/SelectiveEntry.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SelectiveEntry : MonoBehaviour {
+    /// <summary>
+    /// Determines if objects will collide or pass through.
+    /// </summary>
+
+    public List<GameObject> objectsAllowedPassage;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (objectsAllowedPassage.Contains(collision.gameObject))
+        {
+            Physics2D.IgnoreCollision(collision.collider, collision.otherCollider);
+        }
+    }
+}
