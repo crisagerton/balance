9afde8197dbc8c5c1ceee4b7616f9ec66341d1bd
Crystal Agerton <crystal.agerton2015@gmail.com>
2018-08-22 17:03:09 -0700

    Added Title Screen

---
+++ Assets/Scripts/Buttons and Switches/QuitButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour {
+
+    public List<GameObject> players;
+    public KeyCode activationKey;
+
+    private bool playersReady = false;
+
+    private void Update()
+    {
+        if (playersReady && Input.GetKey(activationKey))
+        {
+            Application.Quit();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        foreach (GameObject player in players)
+        {
+            if (collision.gameObject != player)
+                return;
+            playersReady = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playersReady = false;
+    }
+}
